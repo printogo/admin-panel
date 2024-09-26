def716ce96dcc0f7f6d046a6f02a68896da65fef
DavidFarelas <david.farelas.dev@gmail.com>
2024-09-26 16:49:44 -0600

    Correccion error 401

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,6 +5,26 @@ export const login = async (email, password) => {
     email,
     password,
   });
-  if(res.data.user.role === 'ADMIN') return res.data;
+  if (res.data.user.role === 'ADMIN') return res.data;
   throw new Error('Wrong credentials')
 };
+
+export const getUserData = async () => {
+  try {
+    const token = localStorage.getItem('token');
+    if (!token) return false;
+
+    const res = await axiosClient.get("auth/user", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (res.status === 401) {
+      localStorage.removeItem('token')
+      return false;
+    }
+    return true;
+  } catch (error) {
+    throw new Error("Something went wrong")
+  }
+}
--- src/reusable/Auth.js
+++ src/reusable/Auth.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Redirect, Route } from "react-router-dom";
+import { getUserData } from "src/api/auth";
 
 const Auth = ({ path, name, render }) => {
+  const [isLogged, setIsLogged] = useState(true);
+  useEffect(() => {
+    getUserData().then((res) => setIsLogged(res)).catch((err) => console.log(err))
+  }, []);
+
   return (
     <Route
       render={(props) =>
-        localStorage.getItem('token') ? (
+        isLogged ? (
           <Route path={path} name={name} render={render} />
         ) : (
           <Redirect to="/login" />
